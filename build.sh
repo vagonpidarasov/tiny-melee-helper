6e8b4260b48db288dee38d47349cdeeb20205194
Stanislav Beresnev <stanislav.beresnev@ext.addisongloballtd.com>
2019-01-04 17:17:35 +0100

    improve build process to concat lua files

--- build.sh
+++ build.sh
@@ -3,7 +3,12 @@
 rm -rf build &&
 mkdir -p build/TinyMeleeHelper &&
 cp src/* build/TinyMeleeHelper &&
-cd build &&
+cd build/TinyMeleeHelper &&
+cat TinyMeleeHelper.toc | grep lua | xargs cat > tmh.lua &&
+find *.lua ! -name 'tmh.lua' -type f -exec rm -f {} +
+sed -i '' '/.*lua/d' TinyMeleeHelper.toc
+echo tmh.lua >> TinyMeleeHelper.toc
+cd ..
 zip -r TinyMeleeHelper.zip TinyMeleeHelper &&
 cd .. &&
 open build &&
